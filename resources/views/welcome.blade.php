052b39acf5bdac8fac8d8cd4f8491447072befc3
ivamar Junior <hardjunior1@gmail.com>
2020-03-13 02:51:02 +0000

    f

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Raio de laravel
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,84 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/login', function () { //para parametros
+    return "Página de Login";
+})->name('login');
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::middleware([])->group(function(){//'auth'
+//     Route::prefix('admin')->group(function(){
+//          Route::get('/dashboard', function () { //para parametros
+//             return "Página do dashboard";
+//         });
+//         Route::namespace('Admin')->group(function(){
+//             Route::name('Admin.')->group(function(){
+//                 Route::get('/','TesteController@teste')->name('home');
+//                 Route::get('/financeiro',function(){
+//                     return 'Financeiro';
+//                 })->name('financeiro');
+//             });
+//         });
+//     });
+// });
+    // Route::get('/admin/dashboard', function () { //para parametros
+    //     return "Página de Login";
+    // })->middleware('auth');
+
+    Route::group([
+'middleware'=>[],
+'prefix'=>'admin',
+'namespace'=>'admin',
+'name'=>'admin.'
+    ],function(){
+        Route::get('/','TesteController@teste')->name('home');
+        Route::get('/financeiro',function(){
+            return 'Financeiro';
+        })->name('financeiro');
+    });
+// Route::get('/admin/dashboard', function () { //para parametros
+//     return "Página de Login";
+// })->middleware('auth');
+
+
+// Route::post('/contacto', function () {
+//     return "contacto Nao funcionará pois nao é um method post inserido na url";
+// });
+
+// Route::any('/any', function () {
+//     return "any Para qualquer method get ou put";
+// });
+
+// Route::match(["get","post"],'/match', function () {
+//     return "match Para escolher qual será o method get ou put";
+// });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::view('/','welcome',['id'=>'teste']);
+Route::view('/','welcome');
+
+// Route::get('/direcao1', function () { //para parametros
+//     return redirect('/direcao2');
+// });
+Route::get('/direcao1', function () { //para parametros
+    return redirect()->route('nomedadirecao');
 });
+//or Route::redirect('/direcao1','/direcao2');
+
+Route::get('/direcao2', function () {//para parametros
+    return "direcao2";
+})->name('nomedadirecao');
+
+Route::get('/produto/{flag}/podetertambem', function ($flag) {//para parametros
+    return "Testando parametros ".$flag;
+});
+
+Route::get('/produtos/{flag?}', function ($flag='caso seja vazio') {//para parametros
+    return "Testando parametros ".$flag;
+});
+
+Route::get('/categorias/{flag}', function ($flag) {//para parametros
+    return "Testando parametros ".$flag;
+});
+
